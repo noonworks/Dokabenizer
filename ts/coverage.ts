3744dfc8ccff40de8281142ed3f4cdf142186319
noonworks <noonworks@gmail.com>
2017-03-24 23:49:02 +0900

    Add font class.

--- ts/coverage.ts
+++ ts/coverage.ts
@@ -1,8 +1,10 @@
 import * as Frames from './frames';
 import Ticker from './ticker';
+import * as Fonts from './font';
 
 export {Frames};
 export {Ticker};
+export {Fonts};
 
 export class Dummy {
   public static return1(): number {
