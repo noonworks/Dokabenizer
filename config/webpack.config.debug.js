4349c49c48cee56543c670bf1a5bfbf0d1a93edc
noonworks <noonworks@gmail.com>
2017-03-24 23:35:29 +0900

    Add build config.

---
+++ config/webpack.config.debug.js
@@ -0,0 +1,22 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, '..'),
+  entry: './ts/index.ts',
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader'
+      }
+    ]
+  },
+  devtool: 'source-map'
+}
---
+++ config/webpack.config.prod.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, '..'),
+  entry: './ts/index.ts',
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
+}
