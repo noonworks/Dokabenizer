4b91cc698e989d31f96ac4ed2e5bd98186a2ac3b
noonworks <noonworks@gmail.com>
2017-03-24 23:52:36 +0900

    Add panel.

---
+++ ts/index.ts
@@ -0,0 +1,13 @@
+import { Manager } from './frames';
+import { Tile } from './tile';
+import { Panel } from './panel';
+
+function main(): void {
+  const mgr = new Manager(8, 1500, 800, 1700);
+  const tile = new Tile();
+  const panel = new Panel(tile);
+  tile.drawText('AbcD');
+  panel.render(mgr.getAngle(0));
+}
+
+main();
