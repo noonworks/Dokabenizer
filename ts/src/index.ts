8eb6eefc94a74b2a254992bc581f28d37f078da1
noonworks <noonworks@gmail.com>
2017-03-25 00:13:20 +0900

    Fix index.ts and build bundle.js.

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,13 +1,51 @@
-import { Manager } from 'frames';
+import { Setting } from 'setting';
 import { Tile } from 'tile';
 import { Panel } from 'panel';
 
-function main(): void {
-  const mgr = new Manager(8, 1500, 800, 1700);
+/*
+function afterCreated(blob: number): void {
+  let img = document.getElementById('dokabenized_img');
+  if (img === null) {
+    img = document.createElement('img');
+    img.id = 'dokabenized_img';
+    // document.body.appendChild(img);
+  }
+  // img.src = URL.createObjectURL(blob);
+}
+*/
+
+function dokabenize(): void {
+  const setting = new Setting();
+  console.log('dokabenizing...');
   const tile = new Tile();
+  tile.drawText(setting.text);
   const panel = new Panel(tile);
-  tile.drawText('AbcD');
-  panel.render(mgr.getAngle(0));
+  /*
+  const gif = new GIF({
+    workers: 2,
+    quality: 10,
+    workerScript: 'js/gif.worker.js'
+  });
+  */
+  for (let i = 0; i <= setting.manager.maxFrame; i++) {
+    const delay = setting.manager.getDelay(i);
+    if (delay === 0) {
+      continue;
+    }
+    console.log(delay);
+    panel.render(setting.manager.getAngle(0));
+    // gif.addFrame(panel.canvas, {delay: delay, copy: true});
+  }
+  /*
+  gif.on('finished', afterCreated);
+  gif.render();
+  */
 }
 
-main();
+function initialize(): void {
+  const button = <HTMLButtonElement> document.getElementById('dokabenize');
+  button.addEventListener('click', dokabenize, false);
+  console.log('initialized');
+}
+
+addEventListener('load', initialize, false);
